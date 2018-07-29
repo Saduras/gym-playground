8613dcf30693efb74188fc2c3f3478cc54f718b8
David Speck <david@saduras.de>
2018-07-29 18:26:00 +0200

    Tutorial: Print observations and reset if done

--- cart_pole.py
+++ cart_pole.py
@@ -1,8 +1,14 @@
 import gym
 
 env = gym.make('CartPole-v0')
-env.reset()
 
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample())
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print(f"Episode finished after {t+1} timesteps")
+            break
