7a3f50255dcbbc5c2822f2cc3b438d37b7e9a8e0
David Speck <david@saduras.de>
2018-08-02 21:22:37 +0200

    MCC: Re-implement and fix with Q-table, testing on FrozenLake-v0

--- monte_carlo_control.py
+++ monte_carlo_control.py
@@ -1,50 +1,11 @@
 import numpy as np
 import sys
-from sklearn.linear_model import LinearRegression
 
-def epsilon_greedy_policy(Q, observation, epsilon, nA):
-    # equal probability for all actions
-    probs = np.ones(nA, dtype=float) * epsilon / nA
-    # find best action
-    best_action = np.argmax(Q(observation))
-    # increase the probability of the best action to 1 - epsilon
-    probs[best_action] += (1.0 - epsilon)
-    return np.random.choice(nA, p=probs)
+def monte_carlo_control(env, num_episodes, discount_factor=0.95, epsilon=0.1, learning_rate=0.8):
+    Q = np.zeros([env.observation_space.n, env.action_space.n])
 
-def lin_Q(observation, parameters):
-    return np.matmul(observation.transpose(), parameters)
-
-def run_episode(env, policy, render=False):
-    episode = []
-    state = env.reset()
-    total_reward = 0
-    done = False
-    while not done: 
-        action = policy(state)
-        if render:
-            env.render()
-        next_state, reward, done, _ = env.step(action)
-        total_reward += reward
-        episode.append((state, action, reward))
-        state = next_state
-    return episode, total_reward
-    
-def monte_carlo_control(env, num_episodes, discount_factor=1.0, epsilon=0.1, learning_rate=0.1):
-    # Init params for Q function
-    q_params = np.random.rand(env.observation_space.shape[0], env.action_space.n)
-
-    # initialize Q function with random training
-    Q = LinearRegression()
-    rand_X = np.array(np.random.rand(1, env.observation_space.shape[0]))
-    rand_y = np.array(np.random.rand(1, env.action_space.n))
-    print(rand_X.shape)
-    Q.fit(rand_X, rand_y)
-
-    def policy(obs, params): 
-        return epsilon_greedy_policy(lambda x: Q.predict([x]), obs, epsilon=epsilon, nA=env.action_space.n)
-    
-    X_states = []
-    y_rewards = []
+    def policy(obs, i): 
+        return np.argmax(Q[obs,:] + np.random.randn(1, env.action_space.n)*(1./(i+1)))
 
     total_rewards = []
     for i_epsiode in range(1, num_episodes + 1):
@@ -53,39 +14,28 @@ def monte_carlo_control(env, num_episodes, discount_factor=1.0, epsilon=0.1, lea
             print(f"Episode {i_epsiode}/{num_episodes} mean reward {np.mean(total_rewards[-1000:])}")
             sys.stdout.flush()
 
-        # Generate an episode
-        # An episode is an array of (state, action, reward) tuples
-        episode, total_reward = run_episode(env, lambda x: policy(x, q_params))
+        state = env.reset()
+        total_reward = 0
+        done = False
+        while not done: 
+            action = policy(state, i_epsiode)
+            next_state, reward, done, _ = env.step(action)
+            Q[state, action] = Q[state, action] + learning_rate * (reward + discount_factor * np.max(Q[next_state,:]) - Q[state, action])
+            total_reward += reward
+            state = next_state
 
         total_rewards.append(total_reward)
-        
-        # Update Q function
-        for state, action, _ in episode:
-            # Find the first occurance of the (state, action) pair in the episode
-            first_occurence_idx = next(i for i,x in enumerate(episode)
-                                       if x[0].all() == state.all() and x[1] == action)
-            # Sum up all rewards since the first occurance
-            G = sum([x[2]*(discount_factor**i) for i,x in enumerate(episode[first_occurence_idx:])])
 
-            y = Q.predict([state])
-            y[0][action] = G
-            X_states.append(state)
-            y_rewards.append(y[0])
-
-        Q.fit(np.array(X_states), np.array(y_rewards))
-        # print(f"score {Q.score(X_states, y_rewards)}")
-
-    return q_params, policy
+    return total_rewards, policy
 
 def main():
-    observation = np.array([0.1, 0.2, 0.3, 0.4])
-    q_parameters = np.array([[1, 0], [1, 0], [1, 0], [0, 0]])
-    print(f"q_param.shape {q_parameters.shape}")
-    print(f"Q.shape {lin_Q(observation, q_parameters).shape}")
-    
+    num_episodes = 2000
+
     import gym
-    env = gym.make('CartPole-v0')
-    q_params, policy = monte_carlo_control(env, 20000, learning_rate=0.001)
+    env = gym.make('FrozenLake-v0')
+    total_rewards, policy = monte_carlo_control(env, num_episodes, learning_rate=0.8, discount_factor=0.95)
+
+    print(f"Score over time: {sum(total_rewards) / num_episodes}")
 
 if __name__ == '__main__':
     main()
