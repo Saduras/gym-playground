6035255ded2cde19cf94900e48fc3a2985092274
David Speck <david@saduras.de>
2018-08-04 16:09:23 +0200

    MCC: Refactor to clearly seperate policy definition and update from MCC algorithm

--- monte_carlo_control.py
+++ monte_carlo_control.py
@@ -1,8 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
-def monte_carlo_control(env, num_episodes, discount_factor=0.95, epsilon=0.1, learning_rate=0.8):
-    
+def train_mcc(env, num_episodes, discount_factor=0.95, epsilon=0.1, learning_rate=0.8):
     tf.reset_default_graph()
 
     # Feed forward pass of the model
@@ -13,7 +12,7 @@ def monte_carlo_control(env, num_episodes, discount_factor=0.95, epsilon=0.1, le
     # Obtain the loss and update model with gradient descent
     nextQ = tf.placeholder(shape=[1,4], dtype=tf.float32)
     loss = tf.reduce_sum(tf.square(nextQ - Qout))
-    trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+    trainer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
     updateModel = trainer.minimize(loss)
 
     init = tf.initialize_all_variables()
@@ -21,44 +20,53 @@ def monte_carlo_control(env, num_episodes, discount_factor=0.95, epsilon=0.1, le
     with tf.Session() as sess:
         sess.run(init)
 
-        total_rewards = []
-        episode_lengths = []
-        for i_epsiode in range(1, num_episodes + 1):
-            # Print out current episode for debugging
-            if i_epsiode % 1000 == 0:
-                print(f"Episode {i_epsiode}/{num_episodes} mean reward {np.mean(total_rewards[-1000:])}")
+        def policy(state):
+            # Choose an action epsilon-greedy
+            if np.random.rand(1) < epsilon:
+                action = env.action_space.sample()
+            else:
+                Q_values = sess.run(predict, feed_dict={inputs1: np.identity(16)[state:state+1]})
+                action = Q_values[0]
+            return action
 
-            state = env.reset()
-            total_reward = 0
-            done = False
-            length = 0
-            while not done: 
-                length += 1
+        def update_policy(old_state, action, new_state, reward, i_epsiode):
+            # Obtain new Q values
+            targetQ = sess.run(Qout, feed_dict={inputs1: np.identity(16)[old_state:old_state+1]})
+            Q1 = sess.run(Qout, feed_dict={inputs1: np.identity(16)[new_state:new_state+1]})
+            targetQ[0, action] = reward + discount_factor * np.max(Q1)
 
-                # Choose an action epsilon-greedy
-                action, allQ = sess.run([predict, Qout], feed_dict={inputs1: np.identity(16)[state:state+1]})
-                if np.random.rand(1) < epsilon:
-                    action[0] = env.action_space.sample()
-
-                next_state, reward, done, _ = env.step(action[0])
-
-                # Obtain new Q values
-                Q1 = sess.run(Qout, feed_dict={inputs1: np.identity(16)[next_state:next_state+1]})
-                maxQ1 = np.max(Q1)
-                targetQ = allQ
-                targetQ[0, action[0]] = reward + discount_factor * maxQ1
-
-                # Train network using target and predicted Q values
-                _, W1 = sess.run([updateModel, W], feed_dict={inputs1: np.identity(16)[state:state+1], nextQ: targetQ})
-
-                total_reward += reward
-                state = next_state
+            # Train network using target and predicted Q values
+            sess.run([updateModel, W], feed_dict={inputs1: np.identity(16)[old_state:old_state+1], nextQ: targetQ})
 
             # Decay epsilon
             epsilon = 1./((i_epsiode/50) + 10)
 
-            total_rewards.append(total_reward)
-            episode_lengths.append(length)
+        return monte_carlo_control(env, num_episodes, policy, update_policy)
+
+def monte_carlo_control(env, num_episodes, policy, update_policy):
+    total_rewards = []
+    episode_lengths = []
+    for i_epsiode in range(1, num_episodes + 1):
+        # Print out current episode for debugging
+        if i_epsiode % 1000 == 0:
+            print(f"Episode {i_epsiode}/{num_episodes} mean reward {np.mean(total_rewards[-1000:])}")
+
+        state = env.reset()
+        total_reward = 0
+        done = False
+        length = 0
+        while not done: 
+            length += 1
+
+            action = policy(state)
+            next_state, reward, done, _ = env.step(action)
+            update_policy(state, action, next_state, reward, i_epsiode)
+
+            total_reward += reward
+            state = next_state
+
+        total_rewards.append(total_reward)
+        episode_lengths.append(length)
 
     return total_rewards, episode_lengths
 
@@ -67,7 +75,7 @@ def main():
 
     import gym
     env = gym.make('FrozenLake-v0')
-    total_rewards, episode_lengths = monte_carlo_control(env, num_episodes, learning_rate=0.8, discount_factor=0.95)
+    total_rewards, episode_lengths = train_mcc(env, num_episodes, learning_rate=0.1, discount_factor=0.95)
 
     print(f"Score over time: {sum(total_rewards) / num_episodes}")
 
