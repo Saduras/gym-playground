7c9b1fb8f4e65a2b3c5ac13bfa3445f54ae04699
David Speck <david.speck@wooga.net>
2018-08-09 19:59:02 +0200

    Wrap policy and policy update into agent class

--- monte_carlo_control.py
+++ monte_carlo_control.py
@@ -1,47 +1,48 @@
 import numpy as np
 import tensorflow as tf
 
-def train_mcc(env, num_episodes, discount_factor=0.95, epsilon=0.1, learning_rate=0.8):
-    tf.reset_default_graph()
+class agent():
+    def __init__(self, sess, environment, discount_factor=0.95, epsilon=0.1, learning_rate=0.1):
+        self.discount_factor = discount_factor
+        self.epsilon = epsilon
+        self.learning_rate = learning_rate
+        self.sess = sess
+        self.env = environment
 
-    # Feed forward pass of the model
-    inputs1 = tf.placeholder(shape=[1,16], dtype=tf.float32)
-    W = tf.Variable(tf.random_uniform([16,4], 0, 0.01))
-    Qout = tf.matmul(inputs1, W)
-    predict = tf.argmax(Qout, 1)
-    # Obtain the loss and update model with gradient descent
-    nextQ = tf.placeholder(shape=[1,4], dtype=tf.float32)
-    loss = tf.reduce_sum(tf.square(nextQ - Qout))
-    trainer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-    updateModel = trainer.minimize(loss)
+        # Feed forward pass of the model
+        self._inputs1 = tf.placeholder(shape=[1,16], dtype=tf.float32)
+        W = tf.Variable(tf.random_uniform([16,4], 0, 0.01))
+        self._Qout = tf.matmul(self._inputs1, W)
+        self._predict = tf.argmax(self._Qout, 1)
+        # Obtain the loss and update model with gradient descent
+        self._nextQ = tf.placeholder(shape=[1,4], dtype=tf.float32)
+        loss = tf.reduce_sum(tf.square(self._nextQ - self._Qout))
+        trainer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+        self._updateModel = trainer.minimize(loss)
 
-    init = tf.initialize_all_variables()
+        init = tf.global_variables_initializer()
+        self.sess.run(init)
 
-    with tf.Session() as sess:
-        sess.run(init)
+    def predict(self, state):
+        # Choose an action epsilon-greedy
+        if np.random.rand(1) < self.epsilon:
+            action = self.env.action_space.sample()
+        else:
+            Q_values = self.sess.run(self._predict, feed_dict={self._inputs1: np.identity(16)[state:state+1]})
+            action = Q_values[0]
+        return action
 
-        def policy(state):
-            # Choose an action epsilon-greedy
-            if np.random.rand(1) < epsilon:
-                action = env.action_space.sample()
-            else:
-                Q_values = sess.run(predict, feed_dict={inputs1: np.identity(16)[state:state+1]})
-                action = Q_values[0]
-            return action
+    def update_policy(self, old_state, action, new_state, reward, i_epsiode):
+        # Obtain new Q values
+        targetQ = self.sess.run(self._Qout, feed_dict={self._inputs1: np.identity(16)[old_state:old_state+1]})
+        Q1 = self.sess.run(self._Qout, feed_dict={self._inputs1: np.identity(16)[new_state:new_state+1]})
+        targetQ[0, action] = reward + self.discount_factor * np.max(Q1)
 
-        def update_policy(old_state, action, new_state, reward, i_epsiode):
-            # Obtain new Q values
-            targetQ = sess.run(Qout, feed_dict={inputs1: np.identity(16)[old_state:old_state+1]})
-            Q1 = sess.run(Qout, feed_dict={inputs1: np.identity(16)[new_state:new_state+1]})
-            targetQ[0, action] = reward + discount_factor * np.max(Q1)
+        # Train network using target and predicted Q values
+        self.sess.run(self._updateModel, feed_dict={self._inputs1: np.identity(16)[old_state:old_state+1], self._nextQ: targetQ})
 
-            # Train network using target and predicted Q values
-            sess.run([updateModel, W], feed_dict={inputs1: np.identity(16)[old_state:old_state+1], nextQ: targetQ})
-
-            # Decay epsilon
-            epsilon = 1./((i_epsiode/50) + 10)
-
-        return monte_carlo_control(env, num_episodes, policy, update_policy)
+        # Decay epsilon
+        self.epsilon = 1./((i_epsiode/50) + 10)
 
 def monte_carlo_control(env, num_episodes, policy, update_policy):
     total_rewards = []
@@ -74,8 +75,12 @@ def main():
     num_episodes = 10000
 
     import gym
-    env = gym.make('FrozenLake-v0')
-    total_rewards, episode_lengths = train_mcc(env, num_episodes, learning_rate=0.1, discount_factor=0.95)
+    env = gym.make('FrozenLake-v0')    
+
+    tf.reset_default_graph()
+    with tf.Session() as sess:
+        a = agent(sess, env)
+        total_rewards, episode_lengths = monte_carlo_control(env, num_episodes, a.predict, a.update_policy)
 
     print(f"Score over time: {sum(total_rewards) / num_episodes}")
 
