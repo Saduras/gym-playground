de3f2af0df3a1fe410687be50356e443c1198665
David Speck <david.speck@wooga.net>
2018-08-23 19:38:31 +0200

    MCC: Change algo to actual MCC and rewrite cart pole agent

--- monte_carlo_control.py
+++ monte_carlo_control.py
@@ -1,6 +1,5 @@
 import numpy as np
 import tensorflow as tf
-import tensorflow.contrib.slim as slim
 
 class frozen_lake_agent():
     def __init__(self, sess, environment, discount_factor=0.95, epsilon=0.1, learning_rate=0.1):
@@ -45,38 +44,46 @@ class frozen_lake_agent():
         # Decay epsilon
         self.epsilon = 1./((i_epsiode/50) + 10)
 
+### Source: https://github.com/breeko/Simple-Reinforcement-Learning-with-Tensorflow/blob/master/Part%202%20-%20Policy-based%20Agents.ipynb
 class cart_pole_agent():
-    def __init__(self, sess, environment, state_size, action_size, hidden_units, discount_factor=0.95, epsilon=0.1, learning_rate=0.1):
+    def __init__(self, sess, environment, state_size, action_size, hidden_units, discount_factor=0.95, epsilon=0.1, learning_rate=0.1, batch_size=5):
         self.discount_factor = discount_factor
         self.epsilon = epsilon
         self.learning_rate = learning_rate
         self.sess = sess
         self.env = environment
+        self.state_size = state_size
+        self.batch_size = batch_size
 
         # Feed forward pass of the model
-        self.state_in = tf.placeholder(shape=[None, state_size], dtype=tf.float32)
-        hidden_layer = slim.fully_connected(self.state_in, hidden_units, biases_initializer=None, activation_fn=tf.nn.relu)
-        self.output = slim.fully_connected(hidden_layer, action_size, activation_fn=tf.nn.relu)
-        self.chosen_action = tf.argmax(self.output,1)
+        self.observations = tf.placeholder(tf.float32, shape=[None, state_size], name="input_x")
+        
+        W1 = tf.get_variable("W1", shape=[state_size, hidden_units], initializer=tf.contrib.layers.xavier_initializer())
+        layer1 = tf.nn.relu(tf.matmul(self.observations, W1))
 
-        # Obtain the loss and update model with gradient descent
-        self.reward_holder = tf.placeholder(shape=[None], dtype=tf.float32)
-        self.action_holder = tf.placeholder(shape=[None], dtype=tf.int32)
+        W2 = tf.get_variable("W2", shape=[hidden_units, 1], initializer=tf.contrib.layers.xavier_initializer())
+        self.output = tf.nn.sigmoid(tf.matmul(layer1, W2))
 
-        self.indicies = tf.range(0, tf.shape(self.output)[0]) * tf.shape(self.output)[1] + self.action_holder
-        self.responsible_outputs = tf.gather(tf.reshape(self.output, [-1]), self.indicies)
-        self.loss = -tf.reduce_mean(tf.log(self.responsible_outputs) * self.reward_holder)
+        # Backwards pass
+        trainable_vars = [W1, W2]
+        self.input_y = tf.placeholder(tf.float32, shape=[None,1], name="input_y")
+        self.advantages = tf.placeholder(tf.float32, shape=[None], name="reward_signal")
 
-        tvars = tf.trainable_variables()
-        self.gradient_holders = []
-        for idx,var in enumerate(tvars):
-            placeholder = tf.placeholder(tf.float32, name=str(idx)+'_holder')
-            self.gradient_holders.append(placeholder)
+        # Loss function
+        log_lik = tf.log(self.input_y * (self.input_y - self.output) + (1 - self.input_y) * (self.input_y + self.output))
+        loss = -tf.reduce_mean(log_lik * self.advantages)
 
-        self.gradients = tf.gradients(self.loss, tvars)
+        # Gradients
+        self.new_grads = tf.gradients(loss, trainable_vars)
+        self.W1_grad = tf.placeholder(tf.float32, name="batch_grad1")
+        self.W2_grad = tf.placeholder(tf.float32, name="batch_grad2")
 
-        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-        self.update_batch = optimizer.apply_gradients(zip(self.gradient_holders, tvars))
+        self.gradients = np.array([np.zeros(var.get_shape()) for var in trainable_vars])
+
+        # Learning
+        batch_grad = [self.W1_grad, self.W2_grad]
+        adam = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+        self.update_grads = adam.apply_gradients(zip(batch_grad, [W1, W2]))
 
         init = tf.global_variables_initializer()
         self.sess.run(init)
@@ -86,14 +93,37 @@ class cart_pole_agent():
         if np.random.rand(1) < self.epsilon:
             action = self.env.action_space.sample()
         else:
-            a_dist = self.sess.run(self.output, feed_dict={self.state_in:[state]})
+            state = np.reshape(state, [1, self.state_size])
+            a_dist = self.sess.run(self.output, feed_dict={self.observations: state})
             action = np.argmax(a_dist)
         return action
 
-    def update_policy(self, old_state, action, new_state, reward, i_epsiode):
-        feed_dict={self.reward_holder:[reward], self.action_holder:[action], self.state_in:np.vstack([old_state])}
-        grads = self.sess.run(self.gradients, feed_dict=feed_dict)
-        self.sess.run(self.update_batch, feed_dict=dict(zip(self.gradient_holders, grads)))
+    def discount_rewards(self, rewards):
+        """ take 1D float array of rewards and compute discounted reward """
+        discounted_rewards = np.zeros_like(rewards)
+        running_add = 0
+        for t in reversed(range(0, rewards.size)):
+            running_add = running_add * self.discount_factor + rewards[t]
+            discounted_rewards[t] = running_add
+        return discounted_rewards
+
+    def update_policy(self, episode, i_epsiode):
+        episode_array = np.array(episode)
+        old_states = np.stack(episode_array[:,0], axis=0)
+        actions = episode_array[:,1].reshape(-1,1)
+
+        rewards = self.discount_rewards(episode_array[:,3])
+        rewards -= rewards.mean()
+        rewards /= rewards.std()
+
+        self.gradients += np.array(self.sess.run(self.new_grads, feed_dict={
+            self.observations: old_states, 
+            self.input_y: actions, 
+            self.advantages: rewards
+        }))
+        if i_epsiode % self.batch_size == 0:
+            self.sess.run(self.update_grads, feed_dict={self.W1_grad: self.gradients[0], self.W2_grad: self.gradients[1]})
+            self.gradients *= 0
 
         # Decay epsilon
         self.epsilon = 1./((i_epsiode/50) + 10)
@@ -110,15 +140,17 @@ def monte_carlo_control(env, num_episodes, policy, update_policy):
         total_reward = 0
         done = False
         length = 0
+        episode = []
         while not done: 
             length += 1
 
             action = policy(state)
             next_state, reward, done, _ = env.step(action)
-            update_policy(state, action, next_state, reward, i_epsiode)
-
+            episode.append((state, action, next_state, reward))
             total_reward += reward
             state = next_state
+
+        update_policy(episode, i_epsiode)
 
         total_rewards.append(total_reward)
         episode_lengths.append(length)
@@ -133,7 +165,7 @@ def main():
 
     tf.reset_default_graph()
     with tf.Session() as sess:
-        a = cart_pole_agent(sess, env, state_size=4, action_size=2, hidden_units=8)
+        a = cart_pole_agent(sess, env, state_size=4, action_size=2, hidden_units=10, learning_rate=.01, discount_factor=0.99, batch_size=50)
         total_rewards, episode_lengths = monte_carlo_control(env, num_episodes, a.predict, a.update_policy)
 
     print(f"Score over time: {sum(total_rewards) / num_episodes}")
