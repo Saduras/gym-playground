099b2d857d494d04f67e147d82f7af7a476dc736
David Speck <david.speck@wooga.net>
2018-08-24 16:49:58 +0200

    Test: Load proper model

--- test_agent.py
+++ test_agent.py
@@ -4,7 +4,7 @@ def main():
 
     from cart_pole_agent import cart_pole_agent
     agent = cart_pole_agent(state_size=4, action_size=2, hidden_units=10, learning_rate=.01, discount_factor=0.99)
-    agent.load_checkpoint("./checkpoints/cart_pole.ckpt")
+    agent.load_checkpoint("./checkpoints/cart_pole.ckpt-10000")
 
     num_episodes = 10
 
