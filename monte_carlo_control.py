cd32add92a81fec0d3e04c9a4ed826b6b34ddeb4
David Speck <david.speck@wooga.net>
2018-08-25 14:52:50 +0200

    MCC: Make num_episodes and environment a argument

--- monte_carlo_control.py
+++ monte_carlo_control.py
@@ -1,4 +1,6 @@
 import numpy as np
+import gym
+from argparse import ArgumentParser
 
 def monte_carlo_control(env, num_episodes, policy, update_policy, save_checkpoint):
     total_rewards = []
@@ -30,19 +32,31 @@ def monte_carlo_control(env, num_episodes, policy, update_policy, save_checkpoin
 
     return total_rewards, episode_lengths
     
-def main():
-    num_episodes = 10000
+def get_space_size(space):
+    if type(space) is gym.spaces.Box:
+        # TODO: Handle multi-dimensions boxes
+        return space.shape[0]
+    else:
+        return space.n
 
-    import gym
-    env = gym.make('CartPole-v0')    
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--num_eps", help="number of episodes to learn", default=1000, type=int)
+    parser.add_argument("environment", help="name of the gym environment to train on")
+    args = parser.parse_args()
+
+    env = gym.make(args.environment)
+    
+    state_size = get_space_size(env.observation_space)
+    action_size = get_space_size(env.action_space)
 
     from cart_pole_agent import cart_pole_agent
-    agent = cart_pole_agent(state_size=4, action_size=2, hidden_units=10, learning_rate=.01, discount_factor=0.99)
+    agent = cart_pole_agent(state_size=state_size, action_size=action_size, hidden_units=10, learning_rate=.01, discount_factor=0.99)
 
     def save(i_epsiode):
-        agent.save_checkpoint("./checkpoints/cart_pole.ckpt", global_step=i_epsiode)
+        agent.save_checkpoint(f"./checkpoints/{env.name}.ckpt", global_step=i_epsiode)
 
-    total_rewards, episode_lengths = monte_carlo_control(env, num_episodes, agent.sample, agent.train, save)
+    total_rewards, episode_lengths = monte_carlo_control(env, args.num_eps, agent.sample, agent.train, save)
 
     print(f"Overall mean score: {np.mean(total_rewards)} overall mean episode length: {np.mean(episode_lengths)}")
 
