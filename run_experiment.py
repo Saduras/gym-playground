32a99c703269877a6ce875860a347bbfa528eead
David Speck <david.speck@wooga.net>
2018-08-26 16:08:29 +0200

    Run: Create seperate experiment for FrozenLake

--- run_experiment.py
+++ run_experiment.py
@@ -2,9 +2,6 @@ import numpy as np
 import gym
 from argparse import ArgumentParser
 
-from policy_network import policy_network
-from monte_carlo_control import monte_carlo_control
-
 def get_space_size(space):
     if type(space) is gym.spaces.Box:
         # TODO: Handle multi-dimensions boxes
@@ -19,22 +16,49 @@ def main():
     parser.add_argument("environment", help="name of the gym environment to train on")
     args = parser.parse_args()
 
+    experiments = {
+        "FrozenLake-v0": frozen_lake_exp,
+        "CartPole-v0": cart_pole_exp
+    }
+
+    total_rewards, episode_lengths = experiments.get(args.environment)(args)
+
+    print(f"Overall mean score: {np.mean(total_rewards)} overall mean episode length: {np.mean(episode_lengths)}")
+
+def frozen_lake_exp(args):
+    from discrete_policy import discrete_policy
+    from monte_carlo_control import monte_carlo_control
+
     env = gym.make(args.environment)
-    
-    print(env.observation_space)
-    print(env.action_space)
 
     state_size = get_space_size(env.observation_space)
     action_size = get_space_size(env.action_space)
 
-    agent = policy_network(state_size=state_size, action_size=action_size, hidden_units=10, learning_rate=.01, discount_factor=0.99)
+    def save(i_epsiode):
+        agent.save_checkpoint(f"./checkpoints/{args.environment}.ckpt", global_step=i_epsiode)
+
+    agent = discrete_policy(state_size=state_size, action_size=action_size, learning_rate=.1, discount_factor=0.99)
+
+    epsilon = 0.2
+    sample = lambda x : agent.sample_eps_greedy(x, epsilon)
+
+    return monte_carlo_control(env, args.num_eps, args.sum_steps, sample, agent.train, save)
+
+def cart_pole_exp(args):
+    from policy_network import policy_network
+    from monte_carlo_control import monte_carlo_control
+
+    env = gym.make(args.environment)
+
+    state_size = get_space_size(env.observation_space)
+    action_size = get_space_size(env.action_space)
 
     def save(i_epsiode):
         agent.save_checkpoint(f"./checkpoints/{args.environment}.ckpt", global_step=i_epsiode)
 
-    total_rewards, episode_lengths = monte_carlo_control(env, args.num_eps, args.sum_steps, agent.sample, agent.train, save)
+    agent = policy_network(state_size=state_size, action_size=action_size, hidden_units=10, learning_rate=.01, discount_factor=0.99)
 
-    print(f"Overall mean score: {np.mean(total_rewards)} overall mean episode length: {np.mean(episode_lengths)}")
+    return monte_carlo_control(env, args.num_eps, args.sum_steps, agent.sample, agent.train, save)
 
 if __name__ == '__main__':
     main()
